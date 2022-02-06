3e53f8d8a14e3cae431c5a56a1fdbd524baa915c
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2022-02-06 21:13:31 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
